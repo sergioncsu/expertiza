a488cdd1b29ba77322075ec908d70264dc0cb6be
Sergio Vargas Villar <svargas3@ncsu.edu>
2023-10-25 14:20:23 -0400

    replaces the string based condition to hash condition using where. used plunck to retrieve only rhe ta_id values from ta_mappings

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,4 @@
 class Instructor < User
-  # has_many :questionnaires
   has_many :questionnaires, dependent: :nullify
 
   QUESTIONNAIRE = [['My questionnaires', 'list_mine'],
@@ -11,48 +10,38 @@ class Instructor < User
   ASSIGNMENT = [['My assignments', 'list_mine'],
                 ['All public assignments', 'list_all']].freeze
 
+  # Lists all objects of a certain type that are either owned by the user or are public.
   def list_all(object_type, user_id)
-    object_type.where('instructor_id = ? OR private = 0', user_id)
+    object_type.where(instructor_id: user_id).or(object_type.where(private: false))
   end
 
+  # Lists all objects of a certain type that are owned by the user.
   def list_mine(object_type, user_id)
-    object_type.where('instructor_id = ?', user_id)
+    object_type.where(instructor_id: user_id)
   end
 
+  # Gets an object of a certain type that is either owned by the user or is public.
   def get(object_type, id, user_id)
-    # object_type.where("id = ? AND (instructor_id = ? OR private = 0)", id, user_id).first
-    object_type.find_by('id = ? AND (instructor_id = ? OR private = 0)', id, user_id)
+    object_type.find_by(id: id, instructor_id: user_id).or(object_type.find_by(id: id, private: false))
   end
 
+  # Returns the IDs of the TAs for the courses that this instructor teaches.
   def my_tas
-    courses = Course.where(instructor_id: id)
-    ta_ids = []
-    courses.each do |course|
-      ta_mappings = TaMapping.where(course_id: course.id)
-      ta_mappings.each { |mapping| ta_ids << mapping.ta_id } unless ta_mappings.empty?
-    end
+    courses = Course.includes(:ta_mappings).where(instructor_id: id)
+    ta_ids = courses.flat_map { |course| course.ta_mappings.pluck(:ta_id) }
     ta_ids
   end
 
+  # Returns a list of users who have the same or lower privilege level as the given user and are participants in the courses or assignments that the user instructs.
   def self.get_user_list(user)
-    participants = []
     user_list = []
-    # Refactor
-    courses = Course.where(instructor_id: user.id)
-    courses.each do |course|
-      participants << course.get_participants
-    end
-    assignments = Assignment.includes([:participants]).where(instructor_id: user.id)
-    assignments.each do |assignment|
-      participants << assignment.participants
-    end
-    participants.each do |assignment_participants|
-      next if assignment_participants.empty?
-
-      assignment_participants.each do |participant|
-        user_list << participant.user if user.role.has_all_privileges_of?(participant.user.role)
-      end
+    courses = Course.includes(:participants).where(instructor_id: user.id)
+    assignments = Assignment.includes(participants: :user).where(instructor_id: user.id)
+    participants = courses.map(&:participants) + assignments.map(&:participants)
+    participants.flatten.each do |participant|
+      user_list << participant.user if user.role.has_all_privileges_of?(participant.user.role)
     end
     user_list
   end
+
 end
