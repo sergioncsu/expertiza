1c7e3db875deecc092c9014620b7291878198eb0
Sachin R <srdodda@cisco.com>
2023-10-25 00:22:17 -0400

    Refactoring user controller class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
          redirect_to: { action: :list }
 
   def action_allowed?
+    # check if action is allowed
     case params[:action]
     when 'list_pending_requested'
       current_user_has_admin_privileges?
@@ -31,6 +32,7 @@ class UsersController < ApplicationController
   end
 
   def index
+    # Redirect to home page if user is a student, otherwise render the user list.
     if current_user_is_a? 'Student'
       redirect_to(action: AuthHelper.get_home_action(session[:user]), controller: AuthHelper.get_home_controller(session[:user]))
     else
@@ -40,6 +42,7 @@ class UsersController < ApplicationController
   end
 
   def auto_complete_for_user_name
+    # Get available users for the given name input in the user search bar.
     user = session[:user]
     role = Role.find(user.role_id)
     @users = User.where('name LIKE ? and (role_id in (?) or id = ?)', "#{params[:user][:name]}%", role.get_available_roles, user.id)
@@ -48,6 +51,7 @@ class UsersController < ApplicationController
 
   # for anonymized view for demo purposes
   def set_anonymized_view
+    # view page as anonymized
     anonymized_view_starter_ips = $redis.get('anonymized_view_starter_ips') || ''
     session[:ip] = request.remote_ip
     if anonymized_view_starter_ips.include? session[:ip]
@@ -84,7 +88,7 @@ class UsersController < ApplicationController
   def show_if_authorized
     @user = User.find_by(name: params[:user][:name])
     if @user.nil?
-      flash[:note] = params[:user][:name] + ' does not exist.'
+      flash[:note] = "#{params[:user][:name]} does not exist."
       redirect_to action: 'list'
     else
       role
@@ -178,9 +182,10 @@ class UsersController < ApplicationController
   def destroy
     begin
       @user = User.find(params[:id])
-      AssignmentParticipant.where(user_id: @user.id).each(&:delete)
-      TeamsUser.where(user_id: @user.id).each(&:delete)
-      AssignmentQuestionnaire.where(user_id: @user.id).each(&:destroy)
+      # participants will be inherently destroyed
+      # AssignmentParticipant.where(user_id: @user.id).each(&:delete)
+      # TeamsUser.where(user_id: @user.id).each(&:delete)
+      # AssignmentQuestionnaire.where(user_id: @user.id).each(&:destroy)
       # Participant.delete(true)
       @user.destroy
       flash[:note] = "The user \"#{@user.name}\" has been successfully deleted."
